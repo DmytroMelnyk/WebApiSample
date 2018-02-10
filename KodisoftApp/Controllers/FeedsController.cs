e9155636cc33b1dfcdb602c59c784bef7978c41a
Dmytro Melnyk <damelnik87@gmail.com>
2018-02-10 13:57:08 +0200

    UsersController was added

--- KodisoftApp/Controllers/FeedsController.cs
+++ KodisoftApp/Controllers/FeedsController.cs
@@ -24,30 +24,5 @@ namespace KodisoftApp.Controllers
             //var mail = User.FindFirst(x => x.Type == ClaimTypes.Email).Value;
             //return User.Claims.Select(x => x.Value);
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
